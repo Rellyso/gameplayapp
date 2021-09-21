a51721e529f0923c0517fa8b7a8dcc90b54416b2
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-21 20:06:56 -0300

    created CategorySelect component and itself style

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import { View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
+import { categories } from '../../utils/categories'
+import { Category } from '../Category'
 
 import { styles } from './styles'
 
-export function CategorySelect() {
+type Props = {
+  categorySelected: string,
+  setCategory: (categoryId: string) => void
+}
+
+export function CategorySelect({ categorySelected, setCategory }: Props) {
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={true}
       contentContainerStyle={{ paddingRight: 40 }}
-    />
+      style={styles.container}
+    >
+      {categories.map(category => (
+        <Category
+          key={category.id}
+          title={category.title}
+          icon={category.icon}
+          checked={category.id === categorySelected}
+          onPress={() => setCategory(category.id)}
+        />
+      ))}
+    </ScrollView>
   )
 }
